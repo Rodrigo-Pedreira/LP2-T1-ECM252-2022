e0ddb0ba0083e2986289fcee833b000a63e57c1f
Rodrigo-Pedreira <79457805+Rodrigo-Pedreira@users.noreply.github.com>
2022-05-21 20:16:12 -0300

    incoporacao da novo component tela-login

--- angular_front/appt1/src/app/app.module.ts
+++ angular_front/appt1/src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PostagemComponent } from './postagem/postagem.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { TelaLoginComponent } from './tela-login/tela-login.component';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostagemComponent
+    PostagemComponent,
+    TelaLoginComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
