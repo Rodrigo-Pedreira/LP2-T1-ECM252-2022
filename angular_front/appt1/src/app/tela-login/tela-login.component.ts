b694ea25b2e54bdd44d5d4afbd86ac70bb5370fe
Rodrigo-Pedreira <79457805+Rodrigo-Pedreira@users.noreply.github.com>
2022-05-21 23:50:39 -0300

    esqueleto da pagina login criado

---
+++ angular_front/appt1/src/app/tela-login/tela-login.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-tela-login',
+  templateUrl: './tela-login.component.html',
+  styleUrls: ['./tela-login.component.css']
+})
+export class TelaLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  hide_password = true;
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required]);
+
+  getEmailErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+}
