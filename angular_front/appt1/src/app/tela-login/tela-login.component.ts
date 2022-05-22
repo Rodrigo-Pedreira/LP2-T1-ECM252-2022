f0d1cf43751f87e87d26f60120f840a9efbff06d
Rodrigo-Pedreira <79457805+Rodrigo-Pedreira@users.noreply.github.com>
2022-05-22 15:36:07 -0300

    novo FormControl e pronto para usar dados do form

--- angular_front/appt1/src/app/tela-login/tela-login.component.ts
+++ angular_front/appt1/src/app/tela-login/tela-login.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import { Component, OnInit }                 from '@angular/core';
+import {FormControl, FormGroup, Validators } from '@angular/forms';
+// import { from } from 'rxjs';
+
 
 @Component({
   selector: 'app-tela-login',
   templateUrl: './tela-login.component.html',
-  styleUrls: ['./tela-login.component.css']
+  styleUrls:  ['./tela-login.component.css']
 })
 export class TelaLoginComponent implements OnInit {
 
@@ -13,16 +15,22 @@ export class TelaLoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  hide_password = true;
+    hide_password = true;
+    loginForm = new FormGroup({
+      email :    new FormControl('', [Validators.required, Validators.email]),
+      password : new FormControl('', [Validators.required])
+    })
 
-  email = new FormControl('', [Validators.required, Validators.email]);
-  password = new FormControl('', [Validators.required]);
+    get email()    {return this.loginForm.get("email")!}
+    get password() {return this.loginForm.get("password")!}
 
   getEmailErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
-    }
+    if (this.email.hasError('required')) return 'You must enter a value'
+    if (this.email.hasError('email'))    return 'Not a valid email'
+    return ''
+  }
 
-    return this.email.hasError('email') ? 'Not a valid email' : '';
+  onSubmitLogin(){
+    // console.warn(this.loginForm.value) // Debug rodrigo
   }
 }
