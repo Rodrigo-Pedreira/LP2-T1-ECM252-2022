dc1496521645b458cb51f93949c7c3efd92c34fc
Rodrigo-Pedreira <79457805+Rodrigo-Pedreira@users.noreply.github.com>
2022-05-29 21:07:23 -0300

    fix agora export como modulo

--- backend/constants.js
+++ backend/constants.js
@@ -1,14 +1,14 @@
 /** Porta de acesso do servidor. */
-const PORTA_BARRAMENTO_EVENTOS = "10000"
+module.exports.PORTA_BARRAMENTO_EVENTOS = "10000"
 
 /** Porta de acesso das postagens. */
-const PORTA_POSTAGEM = "4000"
+module.exports.PORTA_POSTAGEM = "4000"
 
 /** URL base. Endereço do site. O prefixo da URL*/
-const URL_BASE = "http://localhost"
+module.exports.URL_BASE = "http://localhost"
 
 /** URL para as postagens. */
-const URL_POSTAGEM = "/postagem"
+module.exports.URL_POSTAGEM = "/postagem"
 
 /** URL do barramento de eventos. */
-const URL_BARRAMENTO_EVENTOS = "/eventos"
+module.exports.URL_BARRAMENTO_EVENTOS = "/eventos"
